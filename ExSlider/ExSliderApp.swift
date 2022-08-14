3ece36cdb76cb79de5721ae0e199525a6a0ff430
jake <palatable7@naver.com>
2022-08-14 17:58:20 +0900

    Initial Commit

---
+++ ExSlider/ExSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExSliderApp.swift
+//  ExSlider
+//
+//  Created by 김종권 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct ExSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
