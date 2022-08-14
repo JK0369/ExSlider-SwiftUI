f021c1c1afb769fc35df1a74e0463de475942de0
jake <palatable7@naver.com>
2022-08-14 18:09:50 +0900

    Implement

--- ExSlider/ContentView.swift
+++ ExSlider/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var value = 30.0
+  @State private var isEditing = false
+  
+  var body: some View {
+    VStack {
+      // base
+//      Slider(
+//        value: $value,
+//        in: 0...100
+//      ) { editing in
+//          isEditing = editing
+//        }
+//      Text("\(value)")
+//        .foregroundColor(isEditing ? .red : .blue)
+      
+//      Slider(
+//        value: $value,
+//        in: 0...100,
+//        step: 5
+//      ) { editing in
+//        isEditing = editing
+//      }
+//      Text("\(value)")
+//        .foregroundColor(isEditing ? .red : .blue)
+      
+      Slider(
+        value: $value,
+        in: 0...100,
+        step: 5
+      ) {
+        Text("Title")
+      } minimumValueLabel: {
+        Text("0")
+      } maximumValueLabel: {
+        Text("100")
+      } onEditingChanged: { editing in
+        isEditing = editing
+      }
+      Text("\(value)")
+        .foregroundColor(isEditing ? .red : .blue)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
